99e0ea14875c120280ac422fff78208c404c45a1
LuckyCheng1994 <1991521105@qq.com>
2020-04-08 21:53:14 +0800

    first commit

---
+++ react.js
@@ -0,0 +1,9 @@
+function App(){
+    const [value,setValue] = useState('aaa')
+    return (
+        <div>
+        <InputNumber value={value} onChange={e=>{}}/>
+        <InputNumber defaultValue={value} onChange={e=>{}}/>
+        </div>
+    )
+}
