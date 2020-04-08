500f8e184d00553af1d66591d1ee25cc4d26ef66
LuckyCheng1994 <1991521105@qq.com>
2020-04-08 22:48:43 +0800

    修改

--- react.js
+++ react.js
@@ -1,9 +1,23 @@
-function App(){
-    const [value,setValue] = useState('aaa')
-    return (
-        <div>
-        <InputNumber value={value} onChange={e=>{}}/>
-        <InputNumber defaultValue={value} onChange={e=>{}}/>
-        </div>
-    )
+
+export default class extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value
+        }
+    }
+
+  
+    handleChange(e) {
+        this.props.onChange(e.target.value)
+    }
+
+    render() {
+        return (
+            <div>
+                 <InputNumber value={value} onChange={e=>{value=e.target.value}}/>
+                 <InputNumber defaultValue={value} onChange={e=>this.handleChange(e)} ref={ref => this.input = ref}/>
+            </div>
+        )
+    }
 }
